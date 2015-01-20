c53476c83c00f61fdeb2de61614e50ec4ee2e2c5
ikeikeCoder <lbsneo@gmail.com>
2015-01-20 23:31:34 +0900

    저장소 최초등록

---
+++ src/main/java/org/fervorseed/ikeframework/app/config/AppConfig.java
@@ -0,0 +1,8 @@
+package org.fervorseed.ikeframework.app.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
---
+++ src/main/java/org/fervorseed/ikeframework/common/config/CommonConfig.java
@@ -0,0 +1,18 @@
+package org.fervorseed.ikeframework.common.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+* @package org.fervorseed.ikeframework.common.config
+* @fileName CommonConfig.java
+* 
+* @Company : FervorSeed
+* @Author  : SIM
+* @Date    : 2015. 1. 20. 오후 4:38:42
+* @Version : 1.0
+*/
+@Configuration
+public class CommonConfig {
+
+	
+}
