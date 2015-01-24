aa83a3aa4d80f00d7700d2939986902d577bc013
이병석 <bslee@simplatform.com>
2015-01-24 14:52:58 +0900

    javaConfig 환경설정. web.xml --> Initializer.java 로 변경

---
+++ src/main/java/org/fervorseed/ikeframework/common/config/RootConfig.java
@@ -0,0 +1,23 @@
+package org.fervorseed.ikeframework.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+/**
+* @package org.fervorseed.ikeframework.common.config
+* @fileName RootConfig.java
+* 
+* @Company : FervorSeed
+* @Author  : SIM
+* @Date    : 2015. 1. 21. 오전 9:52:50
+* @Version : 1.0
+* @Description : MVC 설정용 클래스.
+* 						이 클래스는 스프링의 servlet-context.xml 역활을 대신함
+*/
+@EnableWebMvc
+@Configuration
+public class RootConfig {
+
+	
+}
---
+++ src/main/java/org/fervorseed/ikeframework/initializer/Initializer.java
@@ -0,0 +1,68 @@
+package org.fervorseed.ikeframework.initializer;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.fervorseed.ikeframework.app.config.AppConfig;
+import org.fervorseed.ikeframework.common.config.RootConfig;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+
+/**
+* @package org.fervorseed.ikeframework.initializer
+* @fileName Initializer.java
+* 
+* @Company : FervorSeed
+* @Author  : ike
+* @Date    : 2015. 1. 21. 오전 9:41:27
+* @Version : 1.0
+* @Description : WebApplicationInitializer 를 상속하면, 서블릿 컨테이너가 실행될 때 onStartup() 메소드가 자동으로 호출된다.
+* 						이 클래스는 web.xml 의 역할을 대신하거나 보충한다.
+*/
+public class Initializer implements WebApplicationInitializer {
+	
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+		rootContext.register(RootConfig.class);
+		servletContext.addListener(new ContextLoaderListener(rootContext));
+
+		this.addDispatcherServlet(servletContext, "appDispatcher", "/*", "true", 1);
+        this.addUtf8CharacterEncodingFilter(servletContext, "UTF-8", "/*");
+	}
+	
+	/**
+     * Dispatcher Servlet 을 추가한다.
+     * CORS 를 가능하게 하기 위해서 dispatchOptionsRequest 설정을 true 로 한다.
+     * @param servletContext, servletName, pattern, cors, order
+     */
+	private void addDispatcherServlet (ServletContext servletContext, String servletName, String pattern, String cors, int order) {
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext();
+		applicationContext.register(AppConfig.class);
+		//applicationContext.getEnvironment().addActiveProfile("production");
+		
+		 
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet(servletName, new DispatcherServlet(applicationContext));
+        dispatcher.setLoadOnStartup(order);
+        dispatcher.addMapping(pattern);
+        dispatcher.setInitParameter("dispatchOptionsRequest", cors); // CORS 를 위해서 option request 도 받아들인다.
+	}
+	
+	/**
+     * UTF-8 캐릭터 인코딩 필터를 추가한다.
+     * @param servletContext, encoding, pattern
+     */
+    private void addUtf8CharacterEncodingFilter(ServletContext servletContext, String encoding, String pattern)
+    {
+        FilterRegistration.Dynamic filter = servletContext.addFilter("CHARACTER_ENCODING_FILTER", CharacterEncodingFilter.class);
+        filter.setInitParameter("encoding", encoding);
+        filter.setInitParameter("forceEncoding", "true");
+        filter.addMappingForUrlPatterns(null, false, pattern);
+    }
+}
