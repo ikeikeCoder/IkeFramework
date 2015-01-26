dff141d713ceecf5d9ebc069f29c1b14ae23e7b2
이병석 <bslee@simplatform.com>
2015-01-26 15:43:14 +0900

    jdbc 설정 및 마이바티스 설정 추가

--- src/main/java/org/fervorseed/ikeframework/config/web/WebMvcConfig.java
+++ src/main/java/org/fervorseed/ikeframework/config/web/WebMvcConfig.java
@@ -22,8 +22,10 @@ import org.springframework.web.servlet.view.JstlView;
 * @Author  : SIM
 * @Date    : 2015. 1. 24. 오후 5:14:37
 * @Version : 1.0
-* @Description : \@EnableWebMvc 설정 파일
-* 						\@EnableWebMvc 은 xml 설정에서 <mvc:annotation-driven /> 의 역활이다
+* @Description :MVC 설정용 클래스.
+* 						이 클래스는 스프링의 servlet-context.xml 역활을 대신함 
+* 						{@EnableWebMvc} 설정 파일
+* 						{@EnableWebMvc} 은 xml 설정에서 <mvc:annotation-driven /> 의 역활이다
 */
 
 @EnableWebMvc
