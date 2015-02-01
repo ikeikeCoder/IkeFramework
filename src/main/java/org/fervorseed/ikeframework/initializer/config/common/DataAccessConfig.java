44bcadb201740446e2910924e58ae31ea49cf78e
ikeikeCoder <lbsneo@gmail.com>
2015-02-01 10:53:24 +0900

    aspect 라이브러리 추가

--- src/main/java/org/fervorseed/ikeframework/initializer/config/common/DataAccessConfig.java
+++ src/main/java/org/fervorseed/ikeframework/initializer/config/common/DataAccessConfig.java
@@ -9,6 +9,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.stereotype.Repository;
